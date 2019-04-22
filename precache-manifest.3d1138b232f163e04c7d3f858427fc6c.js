8d547daf8caacd2f457581796dbf57f74002f603
martinrrm <martin.ruiz.mares@gmail.com>
2019-04-22 10:02:12 -0500

    Updates

---
+++ precache-manifest.3d1138b232f163e04c7d3f858427fc6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26b2c377fdfaaaaef120",
+    "url": "/martinrrm/static/css/main.4c5b7de4.chunk.css"
+  },
+  {
+    "revision": "26b2c377fdfaaaaef120",
+    "url": "/martinrrm/static/js/main.23d2306e.chunk.js"
+  },
+  {
+    "revision": "be29fc4653b59499628b",
+    "url": "/martinrrm/static/js/runtime~main.c1713da9.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/martinrrm/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/martinrrm/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9367ef6d7786241a947390a0f56d1d82",
+    "url": "/martinrrm/index.html"
+  }
+];
